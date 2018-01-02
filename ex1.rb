7e3411e5650757bdf784a68fb7142e8c9a4c022c
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-01 22:18:28 -0500

    ex3

--- ex1.rb
+++ ex1.rb
@@ -6,14 +6,21 @@ class Person
 def initialize(name)
 @name = name
 @emotions = {
-funny: 1,
-mad: 2,
-sad: 3
+happiness: 1,
+stress: 2,
+fear: 3
 }
 end
 
 def emotion
-  @emotions.each do |emotion, value|
+@emotions.each do |emotion, value|
+if value == 1
+puts "I am feeling #{emotions}!"
+elsif value == 2
+puts "I am feeling #{emotions}!"
+elsif value == 3
+puts "I am feeling #{emotions}!"
+end
 end
 
 
